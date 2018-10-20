1dcac786da8ad4f2a5bad5e3a26dd316a7df0c52
Tyson Hofstetter <tysonh652@gmail.com>
2018-10-20 20:46:58 +1100

    finished logout button

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :movies do
     resources :reviews
     member do 
       get '/reviews', to: 'reviews#index'
     end
   end
+
+  root to: 'movies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
